e1adf9e0d7934db5ad75a7428e5705fe1d3159a7
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-01-16 01:57:39 -0500

    Completed assignment

--- Chapter-2/index.php
+++ Chapter-2/index.php
@@ -9,25 +9,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>1-100</title>
 </head>
 <body>
 
-<script>
-    for (let i = 1; i != 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("Hee Haw!");
-        }
-        else if (i % 3 == 0) {
-            console.log("Hee!");
-        }
-        else if (i % 5 == 0) {
-            console.log("Haw!");
-        }
-        else {
-            console.log(i);
-        }
-    }
-</script>
+
+<script src="functions.js"></script>
 </body>
 </html>
