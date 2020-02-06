826638728a1f6ce51185e37a2ee7441bd611c66f
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-02-06 00:41:11 -0500

    Added necessary files and started on the process

---
+++ Chapter-5/json-search.js
@@ -0,0 +1,21 @@
+let people = [
+    {"name": "John Doe", "sex" : "male", "born" : "2000", "died" : "still alive...", "father" : "Harrison Ford", "Mother" : "Elizabeth Olsen"},
+    {"name": "Jane Doe", "sex" : "female", "born" : "2002", "died" : "still alive...", "father" : "Harrison Ford", "Mother" : "Elizabeth Olsen"},
+    {"name": "Mark Opal", "sex" : "male", "born" : "1750", "died" : "1820", "father" : "Tommy Opal", "Mother" : "Marissa Opal"},
+    {"name": "Jack Black", "sex" : "male", "born" : "1969", "died" : "still alive...", "father" : "Thomas William Black", "Mother" : "Judith Love Cohen"},
+    {"name": "Diana Frances Spencer", "sex" : "female", "born" : "1961", "died" : "1977", "father" : "John Spencer", "Mother" : "Frances Shand Kydd"}
+];
+
+let input = document.getElementById("search");
+let search = document.getElementById("button");
+let output = document.getElementById("output");
+
+console.log(test);
+
+if (input == "hi") {
+    alert("Hello");
+}
+
+function myFunction() {
+
+}
